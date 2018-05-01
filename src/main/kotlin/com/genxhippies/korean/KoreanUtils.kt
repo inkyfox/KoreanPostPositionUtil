6e119f78c01f1bf09a1a9f6a83c5f8e03e5bad78
Yongha Yoo <inkyfox@gmail.com>
2018-05-01 10:55:16 +0900

    add javadoc

--- src/main/kotlin/com/genxhippies/korean/KoreanUtils.kt
+++ src/main/kotlin/com/genxhippies/korean/KoreanUtils.kt
@@ -75,23 +75,128 @@ private fun String.lastLetter(): Char? =
 private infix fun String.append(조사: 조사): String =
         lastLetter()?.let { this + 조사.of(it) } ?: "[$this]${조사.종성_있는_조사}"
 
+/**
+ * "은", "는" 조사를 붙인다.
+ *
+ * @return 명사의 종성이 있으면 "은", 없으면 "는", 알 수 없으면 "은(는)" 문자열이 추가된 문자열
+ */
 val Any?.은는 @JvmName("은는") get() = toString().append(조사.은는)
+
+/**
+ * "이", "가" 조사를 붙인다.
+ *
+ * @return 명사의 종성이 있으면 "이", 없으면 "가", 알 수 없으면 "은(는)" 문자열이 추가된 문자열
+ */
 val Any?.이가 @JvmName("이가") get() = toString().append(조사.이가)
+
+/**
+ * "을", "를" 조사를 붙인다.
+ *
+ * @return 명사의 종성이 있으면 "을", 없으면 "를", 알 수 없으면 "을(를)" 문자열이 추가된 문자열
+ */
 val Any?.을를 @JvmName("을를") get() = toString().append(조사.을를)
+
+/**
+ * "과", "와" 조사를 붙인다.
+ *
+ * @return 명사의 종성이 있으면 "과", 없으면 "와", 알 수 없으면 "과(와)" 문자열이 추가된 문자열
+ */
 val Any?.과와 @JvmName("과와") get() = toString().append(조사.과와)
+
+/**
+ * "으로", "로" 조사를 붙인다.
+ *
+ * @return 명사의 'ㄹ'이 아닌 종성이 있으면 "으로", 없거나 'ㄹ'이면 "로", 알 수 없으면 "(으)로" 문자열이 추가된 문자열
+ */
 val Any?.으로 @JvmName("으로") get() = toString().append(조사.으로)
+
+/**
+ * "이여", "여" 조사를 붙인다.
+ *
+ * @return 명사의 종성이 있으면 "이여", 없으면 "여", 알 수 없으면 "(이)여" 문자열이 추가된 문자열
+ */
 val Any?.이여 @JvmName("이여") get() = toString().append(조사.이여)
 
+/**
+ * 친근함을 표시하는 목적으로 이름의 뒤에 "이"를 붙인다.
+ *
+ * @return 명사의 종성이 있으면 "이", 없으면 빈 문자열, 알 수 없으면 "(이)" 문자열이 추가된 문자열
+ */
 val Any?._이 @JvmName("_이") get() = toString().append(조사._이)
 
-val Any?.은 get() = 은는
-val Any?.는 get() = 은는
-val Any?.이 get() = 이가
-val Any?.가 get() = 이가
-val Any?.을 get() = 을를
-val Any?.를 get() = 을를
-val Any?.과 get() = 과와
-val Any?.와 get() = 과와
-val Any?.로 get() = 으로
-val Any?.여 get() = 이여
-val Any?.`(이)` get() = _이
+/**
+ * `은는`과 같다.
+ *
+ * @See 은는
+ */
+val Any?.은 @JvmName("은") get() = 은는
+
+/**
+ * `은는`과 같다.
+ *
+ * @See 은는
+ */
+val Any?.는 @JvmName("는") get() = 은는
+
+/**
+ * `이가`과 같다.
+ *
+ * @See 이가
+ */
+val Any?.이 @JvmName("이") get() = 이가
+
+/**
+ * `이가`과 같다.
+ *
+ * @See 이가
+ */
+val Any?.가 @JvmName("가") get() = 이가
+
+/**
+ * `을를`과 같다.
+ *
+ * @See 을를
+ */
+val Any?.을 @JvmName("을") get() = 을를
+
+/**
+ * `을를`과 같다.
+ *
+ * @See 을를
+ */
+val Any?.를 @JvmName("를") get() = 을를
+
+/**
+ * `과와`과 같다.
+ *
+ * @See 과와
+ */
+val Any?.과 @JvmName("과") get() = 과와
+
+/**
+ * `과와`과 같다.
+ *
+ * @See 과와
+ */
+val Any?.와 @JvmName("와") get() = 과와
+
+/**
+ * `으로`과 같다.
+ *
+ * @See 으로
+ */
+val Any?.로 @JvmName("로") get() = 으로
+
+/**
+ * `이여`과 같다.
+ *
+ * @See 이여
+ */
+val Any?.여 @JvmName("여") get() = 이여
+
+/**
+ * `_이`과 같다.
+ *
+ * @See _이
+ */
+val Any?.`(이)` @JvmName("_이_") get() = _이
